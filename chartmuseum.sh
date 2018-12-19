912c6a1c5fcb27380c36a593dabc042d0c7332e3
Jörg Henning <Joerg Henning>
2018-12-19 13:26:06 +0800

    Push all charts

--- chartmuseum.sh
+++ chartmuseum.sh
@@ -2,10 +2,12 @@
 
 set -e -o pipefail
 
-echo "[Finding changes]"
-echo "Note: this will not work correctly if a build was restarted - in such cases please push the affected chart manually."
-CHARTS=$(git diff $DRONE_PREV_COMMIT_SHA $DRONE_COMMIT_SHA --name-only | awk -F '/' '{ print $1 }' | uniq)
-echo "$CHARTS"
+echo "[Charts]"
+# This will not work correctly if a build was restarted or a build has failed, was skipped, etc."
+# See https://discourse.drone.io/t/how-to-get-the-commit-range-of-a-push-pr-event/1716/7
+# CHARTS=$(git diff $DRONE_PREV_COMMIT_SHA $DRONE_COMMIT_SHA --name-only | awk -F '/' '{ print $1 }' | uniq)
+CHARTS=$(find . -iname "Chart.yaml" | awk -F "/" '{print $(NF-1)}')
+for CHART in $CHART; do echo $CHART; done
 
 echo "[Using repo ${PLUGIN_HELM_REPO}]"
 helm repo add chartmuseum ${PLUGIN_HELM_REPO}
