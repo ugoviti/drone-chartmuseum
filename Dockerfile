c5c3011c5f5e2825cbf8b295eea4d651ce178f98
Jörg Henning <Joerg Henning>
2018-12-19 10:54:57 +0800

    Refactor to use helm-push

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,13 @@ ENV FILENAME helm-${VERSION}-linux-amd64.tar.gz
 
 COPY chartmuseum.sh /usr/local/bin/chartmuseum.sh
 
-RUN apk add --no-cache bash gawk sed grep bc coreutils curl openssl jq \
+RUN apk add --no-cache bash gawk sed grep bc coreutils git curl openssl jq \
     && chmod +x /usr/local/bin/chartmuseum.sh \
     && curl -sLo /tmp/${FILENAME} http://storage.googleapis.com/kubernetes-helm/${FILENAME} \
     && tar -zxvf /tmp/${FILENAME} -C /tmp \
     && mv /tmp/linux-amd64/helm /bin/helm \
-    && rm -rf /tmp 
+    && rm -rf /tmp
 
+RUN helm init --client-only && helm plugin install https://github.com/chartmuseum/helm-push
 
 ENTRYPOINT /usr/local/bin/chartmuseum.sh
--- chartmuseum.sh
+++ chartmuseum.sh
@@ -1,36 +1,19 @@
 #! /usr/bin/env bash
 
-CURL=$(which curl)
-DIR=$(find . -iname "Chart.yaml" | awk -F "/" '{print $(NF-1)}')
+set -e -o pipefail
 
-helm init --client-only
+CHARTS=$(find . -iname "Chart.yaml" | awk -F "/" '{print $(NF-1)}')
 
-for TARGET in ${DIR}
-do  
-    pushd ${TARGET}
-    ls requirements.yaml > /dev/null 2>&1
-    
-    if [ $? -eq 0 ]; then
-        helm repo add ${PLUGIN_HELM_REPO_NAME} ${PLUGIN_HELM_REPO_ADD}
-        helm dependency update
-    fi
+echo "[Charts to push]"
+echo $CHARTS
 
-    echo ""
-    echo "Create package of ${TARGET} !"
-    MSG=$(helm package .)
-    PACKAGE=$(echo ${MSG} | awk -F "/" '{print $NF}')
-    
-    echo "Upload charts ${PACKAGE}..."
-    RESULT="$("${CURL}" -s --data-binary "@${PACKAGE}" "${PLUGIN_UPLOAD_REPO_URL}/${PLUGIN_PATH}" | jq -r '.[]')"
+echo "[Using repo ${PLUGIN_HELM_REPO}]"
+helm repo add chartmuseum ${PLUGIN_HELM_REPO}
+helm dependency update
 
-    if [ "${RESULT}" == "true" ] || [ "${RESULT}" == "file already exists" ]; then
-        echo "Upload ${PACKAGE} complete !"
-    else 
-        echo ${RESULT}
-        exit 1
-    fi
-    
-    popd
+for CHART in ${CHARTS}; do
+    echo "Pushing ${CHART}"
+    helm push --force ${CHART}/ chartmuseum
 done
 
 exit 0
