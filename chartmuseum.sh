d18861999bc41db3f833b79472f7c34787ff1f66
Jörg Henning <Joerg Henning>
2018-12-19 13:29:51 +0800

    Fix output of charts to be pushed

--- chartmuseum.sh
+++ chartmuseum.sh
@@ -7,7 +7,7 @@ echo "[Charts]"
 # See https://discourse.drone.io/t/how-to-get-the-commit-range-of-a-push-pr-event/1716/7
 # CHARTS=$(git diff $DRONE_PREV_COMMIT_SHA $DRONE_COMMIT_SHA --name-only | awk -F '/' '{ print $1 }' | uniq)
 CHARTS=$(find . -iname "Chart.yaml" | awk -F "/" '{print $(NF-1)}')
-for CHART in $CHART; do echo $CHART; done
+for CHART in $CHARTS; do echo $CHART; done
 
 echo "[Using repo ${PLUGIN_HELM_REPO}]"
 helm repo add chartmuseum ${PLUGIN_HELM_REPO}
